ac3f637e2d0ac701a0b71437d26e93e6c4810d49
unknown <dananad@126.com>
2016-09-05 09:01:46 +0800

    change test file to relative path

--- src/test/java/image/CaptchaIdentifyTest.java
+++ src/test/java/image/CaptchaIdentifyTest.java
@@ -13,15 +13,16 @@ import utils.CustomFileUtil;
 public class CaptchaIdentifyTest {
 	@Test
 	public void batchTestTrueIdentify() throws Exception {
-		String dirPath = CustomFileUtil.getRootPath()+File.separator+ "captchaDir";
-		File dir = new File(dirPath);
+//		String dirPath = CustomFileUtil.getRootPath()+File.separator+ "captchaDir";
+//		File dir = new File(dirPath);
+		File dir = new File(CaptchaIdentifyTest.class.getClassLoader().getResource("captchaDir").getFile());
 		String path = dir.getAbsolutePath();
 		File[] files = dir.listFiles();
 		String[] fileNames = dir.list();
 
 		int length = files.length;
 		for (int i = 0; i < length; i++) {
-			System.out.println(fileNames[i]);
+			System.out.println("遍历到文件:"+fileNames[i]);
 			if (files[i].isDirectory()) {
 				continue;
 			}
